1278af505f1c6430e13592e638f8fccbb4266ca8
Jakkrapat Ampring <guitarpmacc@gmail.com>
2026-03-06 16:26:47 +0700

    fix video error

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -130,14 +130,7 @@
 
     <div class="vsl reveal d4">
       <div style="position:relative;padding-top:56.25%;">
-        <iframe
-          id="hero-bunny-player"
-          src="https://iframe.mediadelivery.net/embed/491305/2fd6de06-22cf-4a36-b2db-e2e84337177c?autoplay=true&loop=false&muted=false&preload=true&responsive=true"
-          loading="lazy"
-          style="border:0;position:absolute;top:0;height:100%;width:100%;"
-          allow="accelerometer;gyroscope;autoplay;encrypted-media;picture-in-picture;"
-          allowfullscreen="true"
-        ></iframe>
+        <iframe src="https://iframe.mediadelivery.net/embed/491305/2fd6de06-22cf-4a36-b2db-e2e84337177c?autoplay=true&loop=false&muted=false&preload=true&responsive=true" loading="lazy" style="border:0;position:absolute;top:0;height:100%;width:100%;" allow="accelerometer;gyroscope;autoplay;encrypted-media;picture-in-picture;" allowfullscreen="true"></iframe>
       </div>
     </div>
 
